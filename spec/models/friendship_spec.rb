5d1c76475fcec6027ea133ebc4e44b140b660aa3
Gustavo Carvalho <gsc13@tuta.io>
2021-03-18 19:03:23 -0300

    Update Friendship unit tests

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -3,12 +3,11 @@ require 'rails_helper'
 RSpec.describe Friendship, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:user_id) }
-    it 'should validate presence of sent_to'
-    it 'should validate presence of status'
+    it { should validate_presence_of(:sent_to_id) }
   end
 
   describe 'associations' do
-    it 'should belongs to user'
-    it 'should belongs to sent_to'
+    it { should belong_to(:user) }
+    it { should belong_to(:sent_to) }
   end
 end
