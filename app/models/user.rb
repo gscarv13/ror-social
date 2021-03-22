d68d82fa2d9957f9d7da4f43615e28b848178c53
Gustavo Carvalho <gsc13@tuta.io>
2021-03-22 18:14:11 -0300

    Fix association

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  has_many :friendships
+  has_many :friendships, dependent: :destroy
   has_many :friends, class_name: 'Friendship', foreign_key: 'sent_to_id'
 
   def friends_list
