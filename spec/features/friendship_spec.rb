9893ca1512220e4909a87b7f1a06171f69babd82
Gustavo Carvalho <gsc13@tuta.io>
2021-03-24 16:58:37 -0300

    Update integration tests

--- spec/features/friendship_spec.rb
+++ spec/features/friendship_spec.rb
@@ -1,28 +1,49 @@
 require 'rails_helper'
 
-RSpec.feature 'Events', type: :feature do
-  before(:all) { FactoryBot.create(:user) }
+def log_in(user)
+  visit new_user_session_path
+  fill_in 'Email', with: user.email
+  fill_in 'Password', with: user.password
 
-  def sign_up(user_name)
-    visit new_user_registration_path
-    within('form') do
-      fill_in 'Name', with: user_name
-      fill_in 'Email', with: "#{user_name}@nw.co"
-      fill_in 'Password', with: '123456789'
-      fill_in 'Password confirmation', with: '123456789'
-    end
+  click_button 'Log in'
+end
 
-    click_button 'Sign up'
+def request_friend(user, cod)
+  log_in(user)
+  visit users_path
+  find(cod).click
+end
+
+RSpec.feature 'Friendship', type: :feature do
+  before(:all) do
+    @elliot = FactoryBot.create(:user, name: 'Elliot', email: 'el@nw.co')
+    @darlene = FactoryBot.create(:user, name: 'Darlene', email: 'da@nw.co')
   end
 
-  context 'request friendship with the first user on user index' do
-    scenario 'should be successful' do
-      sign_up('elliot')
-      visit users_path
-      find('#COD1').click
+  scenario 'request friendship should be successful' do
+    request_friend(@elliot, "#RQ#{@darlene.id}")
+    expect(page).to have_content('Pending')
+    click_link 'Sign out'
+  end
 
-      expect(page).to have_content('Pending')
-      click_link 'Sign out'
-    end
+  scenario 'accept friend request should be successful' do
+    request_friend(@darlene, "#RQ#{@elliot.id}")
+    click_link 'Sign out'
+
+    log_in(@elliot)
+    visit users_path
+    find("#AC#{@darlene.id}").click
+    expect(page).to have_content('Friend')
+  end
+
+  scenario 'refuse friend request should be successful' do
+    @new_user = FactoryBot.create(:user)
+    request_friend(@new_user, "#RQ#{@darlene.id}")
+    click_link 'Sign out'
+
+    log_in(@darlene)
+    visit users_path
+    find("#RF#{@new_user.id}").click
+    expect(page).to have_content('Friend Request')
   end
 end
