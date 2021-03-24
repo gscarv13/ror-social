620a66394d807ded45dede3ad06f00e445500d0d
Gustavo Carvalho <gsc13@tuta.io>
2021-03-24 16:26:57 -0300

    Add friend tag to accepted friendships

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -3,15 +3,17 @@ module FriendshipsHelper
     return unless user != current_user
 
     if current_user.pending_confirmation.include?(user)
-      link_to('Accept', confirm_friendship_path(user), class: 'btn btn-ok', id: "COD#{user.id}") +
-        link_to('Reject', refuse_friendship_path(user), method: :delete, class: 'btn btn-cancel', id: "COD#{user.id}")
+      link_to('Accept', confirm_friendship_path(user), class: 'btn btn-ok', id: "AC#{user.id}") +
+        link_to('Reject', refuse_friendship_path(user), method: :delete, class: 'btn btn-cancel', id: "RF#{user.id}")
     elsif current_user.friendship_requested.include?(user)
       '<span class="btn btn-warning">Pending</span>'.html_safe
     elsif !current_user.friend?(user)
-      link = link_to 'Friend Request', add_friend_path(user), class: 'btn btn-info', id: "COD#{user.id}"
+      link = link_to 'Friend Request', add_friend_path(user), class: 'btn btn-info', id: "RQ#{user.id}"
       op = "<span> #{link} </span>"
 
       op.html_safe
+    else
+      '<span class="btn">Friend</span>'.html_safe
     end
   end
 end
