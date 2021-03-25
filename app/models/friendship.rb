9f9c9a8b669a8ce3ab5f6db7c1e39a14a0c82b64
Gustavo Carvalho <gsc13@tuta.io>
2021-03-25 14:28:02 -0300

    Add reverse friendship row

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -4,4 +4,8 @@ class Friendship < ApplicationRecord
 
   belongs_to :user
   belongs_to :sent_to, class_name: 'User'
+
+  def saved?
+    Friendship.find_by(user_id: user_id, sent_to: sent_to) ? true : false
+  end
 end
